aa71785c5e41c254a2fc447fcd321407ba8af31e
Killian Habasque <killian.habasque@gmail.com>
2024-12-04 19:51:19 +0100

    redirect backend view to front

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<?php get_header(); ?>
+<?php
 
-<main id="main" role="main" id="post-<?php the_ID(); ?>">
-
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-            <?php the_content(); ?>
-        <?php endwhile; ?>
-    <?php endif; ?>
-
-</main>
-
-<?php get_footer(); ?>
+header("Location: https://www.siege-ergonomique.com");
+exit;
